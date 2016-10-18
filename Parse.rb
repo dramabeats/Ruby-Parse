02473be0964afc4a88265785774061556da373a1
Caleb Chitty <Caleb Chitty>
2016-10-18 16:23:26 -0500

    Added comments

--- Parse.rb
+++ Parse.rb
@@ -1,20 +1,20 @@
 require 'net/http'
 require 'open-uri'
-#part of base library
+
+#Download file 
 open("http://s3.amazonaws.com/tcmg412/HTTPLOG") {|f|
   f.each_line {|line| p line}
 }
 
  
-
-
-
-file=HTTPLOG
+#Search file for "get" and count how many
+File = HTTPLOG
 count = 0
 loop do
 	/GET/.match(file)
 	if /GET/.match = true then
 		Count +=1
 	end
+
 end
 
