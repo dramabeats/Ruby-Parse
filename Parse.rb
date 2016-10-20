d3d7afead7c276c57141efd6efc1cb2f0d12b359
Caleb Chitty <Caleb Chitty>
2016-10-20 17:47:58 -0500

    What was the most-requested file? What was the least-requested file?
    Completed using Hash and Making a new sorted array from it

--- Parse.rb
+++ Parse.rb
@@ -5,30 +5,50 @@ require 'open-uri'
 HTTPLOG=open ("http://s3.amazonaws.com/tcmg412/testfile.txt") 
 
  
-#Search file for "GET" and "POST" and count how many
+
 
 requests = 0
 redirects=0
 unsuccessful=0
+filenumbers = Hash.new(0)
+
+#scan for most/least requested file 
+
+
+
+
+
+
+
+
+
 loop do
 	line=HTTPLOG.readline
 
+	#Search file for "GET" and "POST" and count how many
+	
 	if line =~ /GET|POST/ then
 		requests +=1 
 	end
 	
-	#search file for 3xx errors
+	#search file for 3xx errors and count how many
 	
 	if line =~ /\D300\D|\D301\D|\D302\D|\D303\D|\D304\D|\D305\D|\D306\D|\D307\D|\D308\D/ then
 		redirects +=1 
 	end
 	
-	#Search file for 4xx errors
+	#Search file for 4xx errors and count how many
 	
 		if line =~ /\D400\D|\D401\D|\D402\D|\D403\D|\D404\D|\D405\D|\D406\D|\D407\D|\D408\D|\D409\D|\D410\D|\D411\D|\D411\D|
 		\D412\D|\D413\D|\D414\D|\D415\D|\D416\D|\D417\D|\D418\D|\D421\D|\D422\D|\D423\D|\D424\D|\D426\D|\D428\D|\D429\D|\D431\D|\D451\D/ then
 		unsuccessful +=1 
 		end
+		
+	findfile = line[/[a-zA-Z0-9]+\.[a-zA-Z1-9]+/]
+		filenumbers [findfile] +=1
+		
+	
+		
 	
 	if HTTPLOG.eof then
 		break
@@ -38,14 +58,27 @@ loop do
 end
 
 
+hashsorted = filenumbers.sort
+
+
+
+
+
 #output results
-puts "the total number of requests is" 
+puts "The total number of requests is" 
 puts requests
 
-puts "the total number of redirects is" 
+puts "The total number of redirects is" 
 puts redirects
 
-puts "the total number of unsuccessful requests is" 
+puts "The total number of unsuccessful requests is" 
 puts unsuccessful
 
-puts requests/3.0
+puts "The average number of requests per week is about"
+puts  requests/52.0
+
+puts "The most requested file is"
+puts hashsorted.max
+
+puts "The lest requested file is"
+puts hashsorted.min
