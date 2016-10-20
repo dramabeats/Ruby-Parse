c4c02d36ab5858754add108614d8a610498d498b
Caleb Chitty <Caleb Chitty>
2016-10-20 16:15:41 -0500

    Used new test file for 3xx 4xx codes
    1. How many total requests were made in the time period represented in the log?
    2. How many requests were made on each day?
    3. What percentage of the requests were not successful (any 4xx status code)?
    4. What percentage of the requests were redirected elsewhere (any 3xx codes)?
    
    All complete

--- Parse.rb
+++ Parse.rb
@@ -2,18 +2,33 @@ require 'net/http'
 require 'open-uri'
 
 #Download file 
-HTTPLOG=open ("http://s3.amazonaws.com/tcmg412/HTTPLOG") 
+HTTPLOG=open ("http://s3.amazonaws.com/tcmg412/testfile.txt") 
 
  
 #Search file for "GET" and "POST" and count how many
 
-count = 0
+requests = 0
+redirects=0
+unsuccessful=0
 loop do
 	line=HTTPLOG.readline
 
 	if line =~ /GET|POST/ then
-		count +=1 
+		requests +=1 
 	end
+	
+	#search file for 3xx errors
+	
+	if line =~ /\D300\D|\D301\D|\D302\D|\D303\D|\D304\D|\D305\D|\D306\D|\D307\D|\D308\D/ then
+		redirects +=1 
+	end
+	
+	#Search file for 4xx errors
+	
+		if line =~ /\D400\D|\D401\D|\D402\D|\D403\D|\D404\D|\D405\D|\D406\D|\D407\D|\D408\D|\D409\D|\D410\D|\D411\D|\D411\D|
+		\D412\D|\D413\D|\D414\D|\D415\D|\D416\D|\D417\D|\D418\D|\D421\D|\D422\D|\D423\D|\D424\D|\D426\D|\D428\D|\D429\D|\D431\D|\D451\D/ then
+		unsuccessful +=1 
+		end
 	
 	if HTTPLOG.eof then
 		break
@@ -22,21 +37,15 @@ loop do
 
 end
 
-print count 
 
-#search file for 3xx errors
+#output results
+puts "the total number of requests is" 
+puts requests
 
+puts "the total number of redirects is" 
+puts redirects
 
-loop do
-	line=HTTPLOG.readline
+puts "the total number of unsuccessful requests is" 
+puts unsuccessful
 
-	if line =~ /[3][0-9][0-9]/ then
-		count +=1 
-	end
-	
-	if HTTPLOG.eof then
-		break
-	end
-
-
-end
+puts requests/3.0
